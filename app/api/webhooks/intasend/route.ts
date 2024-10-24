17294135ed4234abed13ed31fcfc75ec679404a5
TechMwalugha <emmanuelmwalugha001@gmail.com>
2024-10-24 16:32:01 +0300

    updates

--- app/api/webhooks/intasend/route.ts
+++ app/api/webhooks/intasend/route.ts
@@ -1,5 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(req: NextRequest) {
+    console.log(req.body)
     return new NextResponse(req.body, { status: 200 })
 }
