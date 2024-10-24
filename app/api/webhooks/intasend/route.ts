840dc5a69d25b91f8b4227695c86226a82bd721e
TechMwalugha <emmanuelmwalugha001@gmail.com>
2024-10-24 17:46:09 +0300

    updates

--- app/api/webhooks/intasend/route.ts
+++ app/api/webhooks/intasend/route.ts
@@ -3,5 +3,5 @@ import { NextRequest, NextResponse } from "next/server";
 export async function POST(req: NextRequest) {
     const body = await req.json() 
     console.log(body)
-    return new NextResponse(req.body, { status: 200 })
+    return new NextResponse(body, { status: 200 })
 }
